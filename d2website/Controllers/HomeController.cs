8045390a2a6ca2e6b776555a93638241fa977211
Gudge <GudgePersonal@users.noreply.github.com>
2018-12-25 01:04:55 -0800

    Initial skeleton ASP MVC web app

---
+++ d2website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿// Copyright Small & Fast 2018
+
+namespace d2website.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using d2website.Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ d2website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+// Copyright Small & Fast 2018
+
+namespace d2website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
