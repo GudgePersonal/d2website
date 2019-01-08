ced5c4dd60846a8fd46f3f2670419c1412e9823f
Gudge <GudgePersonal@users.noreply.github.com>
2019-01-07 16:16:44 -0800

    Skeleton controller plus some static files for doing file upload
    Start on some client side JS for reading local csv prior to posting
    to controller.

---
+++ d2website/Controllers/ArmourSelectorController.cs
@@ -0,0 +1,18 @@
+﻿// Copyright Small & Fast 2018
+
+namespace d2website.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ArmourSelectorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- d2website/wwwroot/js/site.js
+++ d2website/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function updateContentDiv(reader)
+
+function handleFile(files) {
+    var div = document.getElementById("main");
+    var file = files[0];
+    div.innerHTML = "<H1>Uploading " + file.name + "</H1>";
+    var reader = new FileReader();
+    //var content = reader.readAsText(file);
+    var contentDiv = doc.getElementById("content");
+    contentDiv.innerHTML = "<H3>" + "Hello World" + "</H3>"
+}
